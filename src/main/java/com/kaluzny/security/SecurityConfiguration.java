5f3ef25bbcef2780fcd1cd49c98c578b36a95583
Oleg <cryoengineer@gmail.com>
2016-11-03 23:06:57 +0200

    Updated CORS filter.

--- src/main/java/com/kaluzny/security/SecurityConfiguration.java
+++ src/main/java/com/kaluzny/security/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.kaluzny.security;
 
+import com.kaluzny.config.CORSFilter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +22,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().fullyAuthenticated();
+        http
+                .addFilterBefore(new CORSFilter(), ChannelProcessingFilter.class)
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()//allow CORS option calls
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .httpBasic();
         http.httpBasic();
         http.csrf().disable();
     }
