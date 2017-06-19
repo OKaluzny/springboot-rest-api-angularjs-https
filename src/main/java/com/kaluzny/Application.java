019abbb23144d0bf46177f6f14d72e93e971ceac
Oleg <cryoengineer@gmail.com>
2017-06-19 17:40:54 +0300

    pom.xml is updated. Added @ComponentScan.

--- src/main/java/com/kaluzny/Application.java
+++ src/main/java/com/kaluzny/Application.java
@@ -5,11 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@ComponentScan("com.kaluzny")
 public class Application {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
