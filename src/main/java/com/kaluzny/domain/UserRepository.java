052680b424097314429ea68eec2a6bc764bb29ab
Oleg <cryoengineer@gmail.com>
2017-06-19 21:39:55 +0300

    UserRepository is updated. Added findByTitle method.

--- src/main/java/com/kaluzny/domain/UserRepository.java
+++ src/main/java/com/kaluzny/domain/UserRepository.java
@@ -3,6 +3,9 @@ package com.kaluzny.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByTitle(String title);
 }
